1afcfb73ecdc43a2ae34d8132d6d31e321f54a35
Andrea Bisognin <andrea.bisognin@gmail.com>
2014-07-17 22:17:57 +0200

    implemented makeCacheMatrix body

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## create a matrix data structure as a list containing function to 
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse of the matrix
+## get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv.x <- NULL
+        set <-function(y) {
+                x<<-y
+                inv.x <<-NULL 
+        }
+        get <-function() x
+        set.inv <- function(inv) inv.x <<- inv
+        get.inv <- function() inv.x
+        list(set=set,get=get,
+             set.inv=set.inv,
+             get.inv=get.inv)
 }
 
 
